824a493568d30dfca53dd421ef0f7fd31b78e15f
Anthony Mwangi <tony.subscryb@gmail.com>
2020-06-24 15:40:16 +0300

    Updates

---
+++ precache-manifest.79e8547bd771687a012d1a4c2d0650d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ccdfa31103280a49651d2e09f6d660",
+    "url": "/health-dashboard/index.html"
+  },
+  {
+    "revision": "ffc4ae9c6f1f681850e6",
+    "url": "/health-dashboard/static/css/main.3d4c9541.chunk.css"
+  },
+  {
+    "revision": "4e724fd368b7f27b1df2",
+    "url": "/health-dashboard/static/js/2.4ba0e267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/health-dashboard/static/js/2.4ba0e267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc4ae9c6f1f681850e6",
+    "url": "/health-dashboard/static/js/main.884a638d.chunk.js"
+  },
+  {
+    "revision": "076261fccd7f414e4f46",
+    "url": "/health-dashboard/static/js/runtime-main.9e9b959c.js"
+  },
+  {
+    "revision": "32114ca0a27166071eea8d1798e6cf22",
+    "url": "/health-dashboard/static/media/avatar-ring.32114ca0.svg"
+  },
+  {
+    "revision": "c7de97c746be12791d57641dcd7ed5e6",
+    "url": "/health-dashboard/static/media/confetti.c7de97c7.svg"
+  },
+  {
+    "revision": "8b7ad89c46dc2a64044890b0b473fff8",
+    "url": "/health-dashboard/static/media/logo.8b7ad89c.svg"
+  },
+  {
+    "revision": "7d34ed450ca3a025aa4752f71ae30306",
+    "url": "/health-dashboard/static/media/menu-button.7d34ed45.svg"
+  }
+]);
